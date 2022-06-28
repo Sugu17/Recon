0af299d0e6fbfb3dcc06df3822806dc94df4eef8
sugu <sugumarsg17@gmail.com>
2022-06-28 15:23:53 +0530

    Created changes for cloud migration
    Signed-off-by: sugu <sugumarsg17@gmail.com>

---
+++ logger/cloud/Recon_API.py
@@ -0,0 +1,70 @@
+import requests
+import json
+from pprint import pprint
+
+class Recon_API:
+    def __init__(self) -> None:
+        self.url="https://3chkfxw8va.execute-api.ap-south-1.amazonaws.com/Recon/dynamodbmanager"
+        self.data={
+                    "operation" : "",
+                    "tableName" : "Recon_Database",
+                    "payload" : {
+                            "Item":
+                                  {
+                                    "metric_name" : "",
+                                    "time_stamp" : 0,
+                                    "value" : ""
+                                  }
+                                }
+                  }
+
+    def success(self,response:requests.Response)->bool:
+        if response.status_code==200:
+            return True
+        if response.status_code==400:
+            return False
+    
+    def get(self,data:dict):
+        self.data['operation']="read"
+        self.data['payload']['Item']=data
+        payload=json.dumps(self.data)
+        response=requests.post(self.url,payload)
+        if self.success(response):
+            print("Retrieved data from the database")
+            pprint(response.content)
+        else:
+            print("\nFailed to read data")
+            pprint(response)
+
+    def post(self,data:dict):
+        self.data['operation']="create"
+        self.data['payload']['Item']=data
+        payload=json.dumps(self.data)
+        response=requests.post(self.url,payload)
+        if self.success(response):
+            print(f"\nLogged {data['metric_name']} with timestamp {data['time_stamp']}")
+
+        else:
+            print("\nFailed to log data")
+            pprint(response.content)
+
+    def delete(self,data:dict):
+        self.data['operation']="delete"
+        self.data['payload']['Item']=data
+        payload=json.dumps(self.data)
+        response=requests.post(self.url,payload)
+        if self.success(response):
+            print(f"\nRemoved {data['metric_name']} with timestamp {data['time_stamp']}")
+
+        else:
+            print("\nFailed to remove data")
+            pprint(response.content)
+            
+if __name__=="__main__":
+    api=Recon_API()
+    data={
+        "metric_name":"Speed",
+        "time_stamp":3230,
+        "value":"3000RPM",
+    }
+    api.post(data)
---
+++ logger/cloud/logger.py
@@ -0,0 +1,44 @@
+from datetime import datetime,timezone
+import obd
+from obd.commands import __mode1__ as cmdlist
+from Recon_API import Recon_API
+from concurrent.futures import ProcessPoolExecutor
+from pprint import pprint
+
+class Recon:
+    def __init__(self) -> None:
+        self.api=Recon_API()
+        self.setup_elm()
+    
+    def setup_elm(self):
+        #silence debug messgages
+        obd.logger.removeHandler(obd.console_handler)
+        #create connection to elm interface
+        path="/dev/pts/3"
+        self.connect=obd.OBD(path)
+        #Check for connection
+        if(self.connect):
+            print("\nConnected to ELM Interface.")
+        else:
+            print("\nUnable to connect, Please check your connection:(")
+
+    def logger(self):
+        with ProcessPoolExecutor(300) as executor:
+            executor.map(self.get_metrics,cmdlist,chunksize=4)
+    
+    def get_metrics(self,cmd):
+        data={}
+        if(self.connect.supports(cmd)):
+            response=self.connect.query(cmd)
+            data["metric_name"]=cmd.desc
+            data["time_stamp"]=int(round(datetime.now(timezone.utc).timestamp()))
+            #data["time_stamp"]=Decimal(datetime.now(timezone.utc).timestamp())
+            data["value"]=str(response.value)
+            #log collected data to database instance
+            self.api.post(data)
+
+if __name__=="__main__":
+    event=Recon()
+    while event.connect.is_connected():
+        event.logger()
+        
